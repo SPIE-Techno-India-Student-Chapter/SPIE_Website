9a5690c979bf1e0de48997a8b3dabf71b60c958d
subidita-262 <msubidita26@gmail.com>
2020-08-19 14:35:09 +0530

    Added the Registration page

--- application.py
+++ application.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 application=app=Flask(__name__)
 
+def store_into_SPIE_table(session):
+	data = Candidates(name = name, email = email, post = post,
+    organisation = organisation, ph_number = ph_number)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/registration")
+def register():
+    return render_template("registration.html")
 
 
 if __name__=="__main__":
